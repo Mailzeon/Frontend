da2f406c3886df42246cfd164c1e6d36c615e018
Mailzeon <divy4ni@gmail.com>
2026-07-04 16:05:38 +0530

    Update layout.tsx

--- app/worker/layout.tsx
+++ app/worker/layout.tsx
@@ -5,9 +5,9 @@ export default function WorkerLayout({ children }: { children: React.ReactNode }
   return (
     <div className="min-h-screen bg-[#0B1120] flex">
       <Sidebar />
-      <div className="flex-1 ml-64 flex flex-col min-h-screen">
+      <div className="flex-1 ml-0 md:ml-64 flex flex-col min-h-screen">
         <Navbar title="Worker Panel" />
-        <main className="flex-1 p-6 animate-fade-in">{children}</main>
+        <main className="flex-1 p-4 md:p-6 animate-fade-in">{children}</main>
       </div>
     </div>
   );

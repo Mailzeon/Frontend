19f55b7fc30e5edbd3089a8d696f8a623b6d4101
Mailzeon <divy4ni@gmail.com>
2026-07-28 14:17:32 +0530

    Update skeleton.tsx

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/lib/utils';
 
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('animate-pulse rounded-xl bg-[#374151]/60', className)} {...props} />;
+  return <div className={cn('animate-pulse rounded-xl bg-white/[0.07]', className)} {...props} />;
 }
 export { Skeleton };
